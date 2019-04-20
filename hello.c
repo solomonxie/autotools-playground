9ed085004165c231128bd880a8a6f6442a5d43a4
Solomon Xie <xiaoboxie@appannie.com>
2019-04-20 15:46:19 +0800

    add more complexity to code for portability configuration

--- hello.c
+++ hello.c
@@ -1,10 +1,18 @@
-/* hello.c: A standard "Hello, world!" program */
- 
+/* hello.c: A program to show the time since the Epoch */
+
 #include <stdio.h>
- 
+#include <sys/time.h>
+
 int main(int argc, char* argv[])
 {
-   printf("Hello, world!\n");
- 
+   double sec;
+   struct timeval tv;
+
+   gettimeofday(&tv, NULL);
+   sec = tv.tv_sec;
+   sec += tv.tv_usec / 1000000.0;
+
+   printf("%f\n", sec);
+
    return 0;
 }
